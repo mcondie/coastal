60962e6991f3f9a07e853b88dbe12ebe4053da63
Matthew Condie <matthew.condie@gmail.com>
2016-04-19 21:28:08 -0600

    small fixes

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,10 +1,10 @@
 class CommentMailer < ApplicationMailer
 	default from: 'comments@coastal.branch13.com',
-					to: 'matthew.condie@gmail.com'
+					to: 'corycoastal@aol.com'
 
 	def comment_email(email, subject, message)
 		@message = message
 		@email = email
-		mail(to: 'matthew.condie@gmail.com', from: 'comments@coastal.branch13.com', cc: email, subject: subject)
+		mail(to: 'corycoastal@aol.com', from: 'comments@coastal.branch13.com', cc: email, subject: subject)
 	end
 end
